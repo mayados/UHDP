0338fbec75586b0a983ce3e2baecb207b966fcc0
mayados <115099504+mayados@users.noreply.github.com>
2023-02-15 17:07:31 +0100

    add type to input / activate option sanitize_html

--- src/Form/MemorialType.php
+++ src/Form/MemorialType.php
@@ -3,21 +3,34 @@
 namespace App\Form;
 
 use App\Entity\AnimalMemorial;
+use App\Entity\CategorieAnimal;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class MemorialType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('sexe')
+            ->add('nom', TextType::class,[
+                'label' => "Nom de l'animal",
+            ])
+            ->add('sexe', ChoiceType::class, [
+                'choices' => [
+                    'Inconnu' => 'Inconnu',
+                    'Male' => 'Male',
+                    'Femelle' => 'femelle',
+                ],
+            ])
             ->add('dateNaissance', DateType::class, [
                 // Pour avoir un mini calendrier à l'affichage
                 'widget' => 'single_text',
@@ -46,11 +59,22 @@ class MemorialType extends AbstractType
                     ])
                 ]
             ])
-            ->add('presentation')
-            ->add('chosesAimees')
-            ->add('chosesDetestees')
-            ->add('histoire')
-            ->add('categorieAnimal')
+            ->add('presentation', TextareaType::class, [
+                'label' => 'Présentation',
+            ])
+            ->add('chosesAimees', TextareaType::class, [
+                'label' => "Ce qu'il / elle aimait"
+            ])
+            ->add('chosesDetestees', TextareaType::class, [
+                'label' => "Ce qu'il / elle détestait"
+            ])
+            ->add('histoire', TextareaType::class, [
+                'label' => "Votre histoire / amitié"
+            ])
+            ->add('categorieAnimal', EntityType::class, [
+                'label' => 'Categorie',
+                'class' => CategorieAnimal::class,
+            ])
             ->add('auteur')
             ->add('submit', SubmitType::class)
         ;
@@ -60,6 +84,7 @@ class MemorialType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => AnimalMemorial::class,
+            'sanitize-html' => true,
         ]);
     }
 }
