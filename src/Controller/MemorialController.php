c04ede65f88bd1a8a87a398ca93f9d5d86f99b73
mayados <115099504+mayados@users.noreply.github.com>
2023-02-15 11:00:42 +0100

    add code in function index to display categories and liste des mémoriaux / create function memoriauxParCategorie()

--- src/Controller/MemorialController.php
+++ src/Controller/MemorialController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\CategorieAnimal;
+use App\Repository\AnimalMemorialRepository;
+use App\Repository\CategorieAnimalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MemorialController extends AbstractController
 {
-    #[Route('/memorial', name: 'app_memorial')]
-    public function index(): Response
+    #[Route('/memoriaux', name: 'app_memoriaux')]
+    public function index(AnimalMemorialRepository $anr, CategorieAnimalRepository $car): Response
     {
-        return $this->render('memorial/index.html.twig', [
-            'controller_name' => 'MemorialController',
+        $listeMemoriaux = $anr->findAll();
+
+        $categories = $car->findAll();
+
+        return $this->render('memorial/listeMemoriaux.html.twig', [
+            'listeMemoriaux' => $listeMemoriaux,
+            'categories' => $categories,
         ]);
     }
+
+    #[Route('/categorie/{id}', name: 'app_categorie')]
+    public function memoriauxParCategorie(AnimalMemorialRepository $anr, CategorieAnimalRepository $car, CategorieAnimal $categorie): Response
+    {
+        $categorie = $car->find($categorie->getId());
+
+        return $this->render('memorial/listeParCategorie.html.twig', [
+            'categorie' => $categorie,
+        ]);
+    }
+
 }
