679de6da94a7c6a5c132cfb14d3429c3456bb5d5
mayados <115099504+mayados@users.noreply.github.com>
2023-02-15 15:39:07 +0100

    add code in function add / add use

--- src/Controller/MemorialController.php
+++ src/Controller/MemorialController.php
@@ -2,13 +2,19 @@
 
 namespace App\Controller;
 
+use App\Form\MemorialType;
 use App\Entity\AnimalMemorial;
 use App\Entity\CategorieAnimal;
+use Doctrine\Persistence\ManagerRegistry;
 use App\Repository\AnimalMemorialRepository;
 use App\Repository\CategorieAnimalRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class MemorialController extends AbstractController
 {
@@ -45,9 +51,45 @@ class MemorialController extends AbstractController
         ]);
     }
 
-    #[Route('/memorial/add', name: 'add_memorial')]
-    public function add(): Response
+    #[Route('/memoriaux/add', name: 'add_memorial')]
+    public function add(ManagerRegistry $doctrine, AnimalMemorial $memorial = null, SluggerInterface $slugger, Request $request): Response
     {
+
+        $form = $this->createForm(MemorialType::class, $memorial);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $memorial = $form->getData();            
+            $imgMemorial = $form->get('imgMemorial')->getData();
+            if($imgMemorial){
+                $originalFileName = pathinfo($imgMemorial->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFileName);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imgMemorial->guessExtension();
+
+                try{
+                    $imgMemorial->move(
+                        $this->getParameter('imgMemorial_directory'),
+                        $newFilename
+                    );
+                }   catch (FileException $e){
+
+                }
+
+                $memorial->setPhoto($newFilename);
+            }
+
+
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($memorial);
+            $entityManager->flush();
+
+
+            return $this->redirectToRoute('app_memoriaux');
+        }
+
+        return $this->render('memorial/add.html.twig', [
+            'formAddMemorial' => $form->createView(),
+        ]);
 
     }
 
