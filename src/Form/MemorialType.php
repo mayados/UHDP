32fdf1e4edb577959f92a35cca7aebbbfd10eb5e
mayados <115099504+mayados@users.noreply.github.com>
2023-02-15 15:40:07 +0100

    add classes and properties

--- src/Form/MemorialType.php
+++ src/Form/MemorialType.php
@@ -5,7 +5,11 @@ namespace App\Form;
 use App\Entity\AnimalMemorial;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class MemorialType extends AbstractType
 {
@@ -14,16 +18,41 @@ class MemorialType extends AbstractType
         $builder
             ->add('nom')
             ->add('sexe')
-            ->add('dateNaissance')
-            ->add('dateDeces')
+            ->add('dateNaissance', DateType::class, [
+                // Pour avoir un mini calendrier à l'affichage
+                'widget' => 'single_text',
+                'label' =>  'Date de naissance',
+            ])
+            ->add('dateDeces', DateType::class, [
+                // Pour avoir un mini calendrier à l'affichage
+                'widget' => 'single_text',
+                'label' =>  'Date de décès',
+            ])
             ->add('lieu')
-            ->add('photo')
+            ->add('imgMemorial', FileType::class, [
+                'label' => "Image de l'animal",
+                // Signifie que ce champ n'est pas associé à une propriété de l'entité AnimalMemorial
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    /* On ne peut pas définir la validation des champs non mappés 
+                    en utilisant des annontaions dans l'entité, On utilise donc les classes de contrainte php */
+                    new File([
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Veuillez choisir un format valide (jpeg, jpg, png)',
+                    ])
+                ]
+            ])
             ->add('presentation')
             ->add('chosesAimees')
             ->add('chosesDetestees')
             ->add('histoire')
             ->add('categorieAnimal')
             ->add('auteur')
+            ->add('submit', SubmitType::class)
         ;
     }
 
