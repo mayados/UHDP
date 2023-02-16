67246f0f4c1c7253b2e7e740828b7e29f22f2f49
mayados <115099504+mayados@users.noreply.github.com>
2023-02-16 18:15:10 +0100

    Created service for images uploaded

---
+++ src/Service/UploaderService.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class UploaderService
+{
+    private $params;
+    private $slugger;
+
+    public function __construct(ParameterBagInterface $params, SluggerInterface $slugger)
+    {
+        $this->params = $params;
+        $this->slugger = $slugger;
+    }
+
+    public function add(UploadedFile $image, ?string $folder = '')
+    {
+        $originalFileName = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $this->slugger->slug($originalFileName);
+        $newFilename = $safeFilename.'-'.uniqid().'.'.$image->guessExtension();
+
+        try{
+            $image->move(
+                $this->params->get('images_directory') . $folder,
+                $newFilename
+            );
+        }   catch (FileException $e){
+
+        }
+
+        return $newFilename;
+    }
+
+    // Pour supprimer une image dans le dossier en question 
+    public function delete(string $fichier, ?string $folder = '')
+    {
+        // Si le fichier est différent de l'image par défaut
+        if($fichier !== 'default.jpg'){
+            $path = $this->params->get('images_directory') . $folder;
+            $imageASupprimer = $path . '/' . $fichier;
+            if(file_exists($imageASupprimer)){
+                unlink($imageASupprimer);
+            }
+
+        }
+
+
+    }
+}
+
+?>
