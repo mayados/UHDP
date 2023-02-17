6c2f2f7e58cd0d0310331dada5eff9392d1a7c25
mayados <115099504+mayados@users.noreply.github.com>
2023-02-17 17:02:22 +0100

    create function index

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TopicRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class ForumController extends AbstractController
 {
     #[Route('/forum', name: 'app_forum')]
-    public function index(): Response
+    public function index(TopicRepository $tr): Response
     {
+
+        $topics = $tr->findBy([],['dateCreation' => 'ASC']);
+        
         return $this->render('forum/index.html.twig', [
-            'controller_name' => 'ForumController',
+            'topics' => $topics,
         ]);
     }
 }
