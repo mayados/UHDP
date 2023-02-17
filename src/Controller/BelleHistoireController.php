c297ef02607e9cba8380fcc203267b658594f612
mayados <115099504+mayados@users.noreply.github.com>
2023-02-17 14:43:33 +0100

    add functions index() / showHistoire() / addHistoire() /

--- src/Controller/BelleHistoireController.php
+++ src/Controller/BelleHistoireController.php
@@ -2,17 +2,108 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\HistoireType;
+use App\Entity\BelleHistoire;
+use App\Service\UploaderService;
+use App\Form\CommentHistoireType;
+use App\Entity\CommentBelleHistoire;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\BelleHistoireRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BelleHistoireController extends AbstractController
 {
-    #[Route('/belle/histoire', name: 'app_belle_histoire')]
-    public function index(): Response
+    #[Route('/bellesHistoires', name: 'app_belles_histoires')]
+    public function index(BelleHistoireRepository $bhr): Response
     {
-        return $this->render('belle_histoire/index.html.twig', [
-            'controller_name' => 'BelleHistoireController',
+
+        $listeHistoires = $bhr->findAll();
+
+        return $this->render('belle_histoire/bellesHistoires.html.twig', [
+            'listeHistoires' => $listeHistoires,
         ]);
+
+    }
+
+    #[Route('/bellesHistoires/histoire/{id}', name: 'show_histoire')]
+    public function showHistoire(BelleHistoireRepository $bhr, BelleHistoire $histoire, ManagerRegistry $doctrine, Request $request): Response
+    {
+        $histoire = $bhr->find($histoire->getId());
+
+        $commentaire = new CommentBelleHistoire();
+        $form = $this->createForm(CommentHistoireType::class, $commentaire);
+        $form->handleRequest($request); 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $commentaire = $form->getData();  
+            $dateNow = new \DateTime();
+            $commentaire->setDateCreation($dateNow);
+            $histoire->addCommentaire($commentaire);  
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($commentaire);
+            $entityManager->flush();                                  
+
+            return $this->redirectToRoute(
+                'show_histoire',
+                ['id' => $histoire->getId()]
+            );
+        }
+
+        return $this->render('belle_histoire/belleHistoire.html.twig', [
+            'histoire' => $histoire,
+            'formAddComment' => $form->createView(),
+        ]);
+        
+    }
+
+    #[Route('/bellesHistoires/add', name: 'add_histoire')]
+    #[Route('/bellesHistoires/edit/{id}', name: 'edit_histoire')]
+    public function addHistoire(BelleHistoireRepository $bhr, BelleHistoire $histoire = null, UploaderService $uploaderService, ManagerRegistry $doctrine, Request $request): Response
+    {
+        $edit = false;
+        if($histoire){
+            $edit = true;
+        }
+
+        if(!$histoire){
+            $histoire = new BelleHistoire();
+        }
+
+        $form = $this->createForm(HistoireType::class, $histoire);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $memorial = $form->getData();            
+            $imgHistoire = $form->get('imgHistoire')->getData();
+            if($imgHistoire){
+                // Si on est dans le cas d'un edit et qu'une nouvelle image est uploadée (car lors d'un ajout on ne va pas supprimer le fichier qu'ion crée..)
+                if($histoire->getPhoto() != null){
+                    // On cherche la photo stockée pour le mémorial correspondant
+                    $previousPhoto = $histoire->getPhoto();
+
+                    $folder = 'imgHistoire';
+                    $uploaderService->delete($previousPhoto,$folder);
+                }
+                // Dans le cas où il y a une image soumise mais que le mémorial n'a pas encore d'image (=> cas d'ajout de mémorial ou edit sans image)
+                $folder = 'imgHistoire';
+                $image = $uploaderService->add($imgHistoire,$folder);
+                $memorial->setPhoto($image);                              
+            }
+
+            // Dans tous les cas, on persist le memorial
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($histoire);
+            $entityManager->flush();
+
+
+            return $this->redirectToRoute('app_belles_histoires');
+        }
+
+        return $this->render('belle_histoire/add.html.twig', [
+            'formAddHistoire' => $form->createView(),
+        ]);
+        
     }
 }
