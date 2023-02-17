96308756d19f174c1fc51d32c8ec65b0dc3ebf02
mayados <115099504+mayados@users.noreply.github.com>
2023-02-17 11:01:46 +0100

    modify length of nom property

--- src/Entity/AnimalMemorial.php
+++ src/Entity/AnimalMemorial.php
@@ -16,7 +16,7 @@ class AnimalMemorial
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 100)]
+    #[ORM\Column(length: 50)]
     private ?string $nom = null;
 
     #[ORM\Column(length: 10, nullable: true)]
