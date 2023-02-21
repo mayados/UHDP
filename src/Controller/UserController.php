15725122ce74338d9cd4a32ea8a0343e94e3a6a2
mayados <115099504+mayados@users.noreply.github.com>
2023-02-21 15:14:31 +0100

    add functions showProfile() and banUser()

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
@@ -15,4 +18,37 @@ class UserController extends AbstractController
             'controller_name' => 'UserController',
         ]);
     }
+
+    #[Route('/user/{id}', name: 'show_profile')]
+    public function showProfile(UserRepository $ur, User $user): Response
+    {
+        $user = $ur->find($user->getId());
+
+        return $this->render('user/profil.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/user/ban/{id}', name: 'ban_user')]
+    public function banUser(User $user, UserRepository $ur, ManagerRegistry $doctrine)
+    {
+        $entityManager = $doctrine->getManager();
+        $user = $ur->find($user->getId());
+        // On vérifie s'il s'agit d'un bannissement ou débannissement
+        if($user->isBannir() == false){
+            // On ban
+            $user->setBannir(true);
+        }else{
+            // On dé-ban
+            $user->setBannir(false);
+        }
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute(
+            'show_profile',
+            ['id' => $user->getId()]
+        );
+    }
+
 }
