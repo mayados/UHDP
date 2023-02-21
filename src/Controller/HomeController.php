55bb47bdc7adf37e79229d69932e72e13a44de67
mayados <115099504+mayados@users.noreply.github.com>
2023-02-21 12:02:48 +0100

    add code to display last belles histoires

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\AnimalMemorialRepository;
+use App\Repository\BelleHistoireRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(AnimalMemorialRepository $amr): Response
+    public function index(AnimalMemorialRepository $amr, BelleHistoireRepository $bhr): Response
     {
         $derniersMemoriaux = $amr->findBy([],['dateCreation' => 'DESC'],3);
+        $dernieresHistoires = $bhr->findBy([],['dateCreation' => 'DESC'],3);
 
         return $this->render('home/index.html.twig', [
             'derniersMemoriaux' => $derniersMemoriaux,
+            'dernieresHistoires' => $dernieresHistoires,
         ]);
     }
 }
