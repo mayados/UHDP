9e8090272f2b301aeadd142142a382ad5e0954a9
mayados <115099504+mayados@users.noreply.github.com>
2023-02-21 10:02:35 +0100

    function index() to display lasts memoriaux

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AnimalMemorialRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(AnimalMemorialRepository $amr): Response
     {
+        $derniersMemoriaux = $amr->findBy([],['dateCreation' => 'DESC'],3);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'derniersMemoriaux' => $derniersMemoriaux,
         ]);
     }
 }
