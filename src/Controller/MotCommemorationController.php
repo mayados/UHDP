49ff9863fd58c3cb10450facb8c6480eb758adf6
mayados <115099504+mayados@users.noreply.github.com>
2023-02-21 10:04:03 +0100

    create public function index() to display motCommemoration and display form to create a mot

--- src/Controller/MotCommemorationController.php
+++ src/Controller/MotCommemorationController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\MotType;
+use App\Entity\MotCommemoration;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\MotCommemorationRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MotCommemorationController extends AbstractController
 {
     #[Route('/mot/commemoration', name: 'app_mot_commemoration')]
-    public function index(): Response
+    public function index(MotCommemorationRepository $mcr, ManagerRegistry $doctrine, Request $request): Response
     {
-        return $this->render('mot_commemoration/index.html.twig', [
-            'controller_name' => 'MotCommemorationController',
+        // On trouve tous les mots par ordre décroissant pour afficher le dernier écrit au-dessus
+        $mots = $mcr->findBy([],['dateCreation' => 'DESC']);
+
+        // On veut afficher le formulaire pour ajouter un mot depuis cette même page
+        $mot = new MotCommemoration();
+        $date = new \DateTime();     
+        $form = $this->createForm(MotType::class, $mot);
+        $form->handleRequest($request); 
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $mot = $form->getData();  
+            $mot->setDateCreation($date);
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($mot);
+            $entityManager->flush();                                  
+
+            return $this->redirectToRoute(
+                'app_mot_commemoration',
+            );
+        }
+
+        return $this->render('mot_commemoration/mur.html.twig', [
+            'mots' => $mots,
+            'formAddMot' => $form->createView(),
         ]);
     }
+
 }
