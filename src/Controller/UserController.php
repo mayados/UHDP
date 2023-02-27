66207886b5f4ce2f76648c04a7d32db3699d4609
mayados <mariannedos.cours@gmail.com>
2023-02-27 16:41:04 +0100

    delete functions findUsers() and banUser()

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -19,20 +19,6 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/users', name: 'app_users')]
-    public function findUsers(UserRepository $ur): Response
-    {
-        // Renvoie une erreur 400 : acces denied si le user qui tente d'y accéder n'a pas le rôle admin
-        $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        $utilisateursBannis = $ur->findBannedUsersNotAdmin();
-        $utilisateursNonBannis = $ur->findNotBannedUsersNotAdmin();
-
-        return $this->render('admin/users.html.twig', [
-            'utilisateursBannis' => $utilisateursBannis,
-            'utilisateursNonBannis' => $utilisateursNonBannis,
-        ]);
-    }
-
     #[Route('/user/{id}', name: 'show_profile')]
     public function showProfile(UserRepository $ur, User $user): Response
     {
@@ -54,28 +40,6 @@ class UserController extends AbstractController
         // Si le user n'est pas connecté, on dirige vers la page de login
         return $this->redirectToRoute('app_login'); 
 
-    }
-
-    #[Route('/user/ban/{id}', name: 'ban_user')]
-    public function banUser(User $user, UserRepository $ur, ManagerRegistry $doctrine)
-    {
-        $entityManager = $doctrine->getManager();
-        $user = $ur->find($user->getId());
-        // On vérifie s'il s'agit d'un bannissement ou débannissement
-        if($user->isBannir() == false){
-            // On ban
-            $user->setBannir(true);
-        }else{
-            // On dé-ban
-            $user->setBannir(false);
-        }
-
-        $entityManager->flush();
-
-        return $this->redirectToRoute(
-            'show_profile',
-            ['id' => $user->getId()]
-        );
     }
 
 }
