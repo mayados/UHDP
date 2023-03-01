886cf799ebbff60fa5d708b2e2e102247c0ede59
mayados <115099504+mayados@users.noreply.github.com>
2023-03-01 17:20:23 +0100

    create function editProfile to modify pseudo and profil picture

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,11 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Repository\AnimalMemorialRepository;
-use App\Repository\BelleHistoireRepository;
-use App\Repository\TopicRepository;
+use App\Form\UserType;
+use App\Service\UploaderService;
 use App\Repository\UserRepository;
+use App\Repository\TopicRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\BelleHistoireRepository;
+use App\Repository\AnimalMemorialRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -52,6 +55,54 @@ class UserController extends AbstractController
 
         // Si le user n'est pas connecté, on dirige vers la page de login
         return $this->redirectToRoute('app_login'); 
+
+    }
+
+    #[Route('/user/edit/{id}', name: 'edit_profile')]
+    public function editProfile(User $user, UploaderService $uploaderService, Request $request, ManagerRegistry $doctrine)
+    {
+
+        if(!$this->getUser()){
+            return $this->redirectToRoute('app_login');
+        }
+
+        if(!$this->getUser() === $user){
+            return $this->redirectToRoute('app_home');
+        }
+
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {         
+            $imgProfil = $form->get('photoProfil')->getData();
+            if($imgProfil){
+                // Si on est dans le cas d'un edit et qu'une nouvelle image est uploadée (car lors d'un ajout on ne va pas supprimer le fichier qu'ion crée..)
+                if($user->getPhoto() != null){
+                    $previousPhoto = $user->getPhoto();
+                    $folder = 'imgUserProfil';
+                    $uploaderService->delete($previousPhoto,$folder);
+                }
+                    // Dans le cas où il y a une image soumise mais que le profil n'a pas encore d'image (=> cas d'edit sans image)
+                $folder = 'imgUserProfil';
+                $image = $uploaderService->add($imgProfil,$folder);
+                $user->setPhoto($image);                              
+                }
+
+                $entityManager = $doctrine->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                return $this->redirectToRoute(
+                    'show_profile',
+                    ['id' => $user->getId()]
+                );
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'formEditProfile' => $form->createView(),
+        ]); 
 
     }
 
