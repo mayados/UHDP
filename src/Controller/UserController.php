2dd06987e29bbe316e955b476a7059ff280097b2
mayados <115099504+mayados@users.noreply.github.com>
2023-03-01 15:24:40 +0100

    add code in function showProfile() to display memoriaux / histoires / topics of the user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\AnimalMemorialRepository;
+use App\Repository\BelleHistoireRepository;
+use App\Repository\TopicRepository;
 use App\Repository\UserRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,19 +23,29 @@ class UserController extends AbstractController
     }
 
     #[Route('/user/{id}', name: 'show_profile')]
-    public function showProfile(UserRepository $ur, User $user): Response
+    public function showProfile(UserRepository $ur, User $user, AnimalMemorialRepository $amr, BelleHistoireRepository $bhr, TopicRepository $tr): Response
     {
         // Il faut être connecté pour accéder à son profil ou à la page de profil d'un utilisateur
         if($this->getUser()){
             // En fonction de si le user est le même que le current ou non, on affiche des render différents
             $user = $ur->find($user->getId());
+            $memoriaux = $amr->findBy(['auteur' => $user],['dateCreation' => 'DESC']);               
+            $histoires = $bhr->findBy(['auteur' => $user],['dateCreation' => 'DESC']);               
+            $topics = $tr->findBy(['auteur' => $user],['dateCreation' => 'DESC']);               
             if($this->getUser() != $user){
+              
                 return $this->render('user/profil.html.twig', [
                     'user' => $user,
+                    'memoriaux' => $memoriaux,
+                    'histoires' => $histoires,
+                    'topics' => $topics,
                 ]);            
             }else{
                 return $this->render('user/monProfil.html.twig', [
                     'user' => $user,
+                    'memoriaux' => $memoriaux,
+                    'histoires' => $histoires,
+                    'topics' => $topics,
                 ]); 
             }            
         }
