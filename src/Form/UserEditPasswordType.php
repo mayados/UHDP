88cd0a2323ead818d439880fd9b30dcd9867efb1
mayados <mariannedos.cours@gmail.com>
2023-03-02 11:03:29 +0100

    Modify password

---
+++ src/Form/UserEditPasswordType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class UserEditPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('currentPassword', PasswordType::class, [
+                'required' => true,
+                'label' => 'Mot de passe actuel',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner votre mot de passe actuel',
+                    ]),
+                ],   
+            ])
+            ->add('newPassword', RepeatedType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent correspondre.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Nouveau mot de passe',
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez renseigner un mot de passe',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Votre mot de passe doit faire un minimum {{ limit }} caractères',
+                            'max' => 50,
+                        ]),
+                    ],                    
+                ],
+                'second_options' => ['label' => 'Répéter le nouveau mot de passe'],
+
+            ])
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
