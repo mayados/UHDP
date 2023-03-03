bea5f7672864b56b29a6b3c3bc77157d413b8466
mayados <115099504+mayados@users.noreply.github.com>
2023-03-03 12:02:26 +0100

    add uniqueEntity

--- src/Entity/Topic.php
+++ src/Entity/Topic.php
@@ -2,14 +2,16 @@
 
 namespace App\Entity;
 
-use App\Repository\TopicRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\TopicRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: TopicRepository::class)]
+#[UniqueEntity(fields: ['titre'], message: 'Il y a déjà un topic avec ce titre')]
 class Topic
 {
     #[ORM\Id]
@@ -17,7 +19,7 @@ class Topic
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     #[Assert\NotBlank()]
     #[Assert\Length(
         min: 2,
