d7dcacb7506bf1415ad47533be0467297bc0edca
mayados <mariannedos.cours@gmail.com>
2023-03-07 10:47:25 +0100

    create SearchType form to add a searchbar

---
+++ src/Form/SearchType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class SearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('q', TextType::class, [
+                'label' => false,
+                'attr' => [
+                'placeholder' => 'Rechercher un mémorial'                 
+                ],
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'La recherche ne peut pas être nulle']),
+                ]
+            ])
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'method' => 'GET',
+        ]);
+    }
+}
