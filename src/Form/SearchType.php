986a864fb827cec24db20a97f11f44fddfbaa012
mayados <mariannedos.cours@gmail.com>
2023-03-09 16:53:20 +0100

    comment submit for test

--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -55,7 +55,7 @@ class SearchType extends AbstractType
                 'required' => false,
 
             ])
-            ->add('submit', SubmitType::class)
+            // ->add('submit', SubmitType::class)
         ;
     }
 
