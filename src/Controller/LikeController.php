1f8a2951fb6dbd2a3792a4e1029095f2a8796fa4
mayados <115099504+mayados@users.noreply.github.com>
2023-03-23 13:19:29 +0100

    add methods likeComment() and showSoutient()

--- src/Controller/LikeController.php
+++ src/Controller/LikeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\AnimalMemorial;
 use App\Entity\BelleHistoire;
+use App\Entity\CommentBelleHistoire;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -36,6 +38,64 @@ class LikeController extends AbstractController
         return $this->json([
             'message' => 'Le like a été ajouté.',
             'nbLike' => $histoire->howManyLikes()
+        ]);
+    }
+
+    #[Route('/like/commentBelleHistoire/{id}', name: 'app_likeComment')]
+    public function likeComment(CommentBelleHistoire $comment, EntityManagerInterface $manager): Response
+    {
+
+        $user = $this->getUser();
+
+        // On vérifie si le User est contenu dans la collection de likes d'utilisateurs du commentaire en question
+        if($comment->isLikedByUser($user))
+        {
+            // Si il a déjà liké, on lui dit de remove le like
+            $comment->removeLike($user);
+            $manager->flush();
+
+            // On précise les datas que l'on souhaite envoyer au json
+            return $this->json([
+                'message' => 'Le like de commentaire a été supprimé.',
+                'nbLike' => $comment->howManyLikes()
+            ]);
+        }
+
+        $comment->addLike($user);
+        $manager->flush();
+
+        return $this->json([
+            'message' => 'Le like de commentaire a été ajouté.',
+            'nbLike' => $comment->howManyLikes()
+        ]);
+    }
+
+    #[Route('/like/memorial/{id}', name: 'app_showSoutient')]
+    public function showSoutient(AnimalMemorial $memorial, EntityManagerInterface $manager): Response
+    {
+
+        $user = $this->getUser();
+
+        // On vérifie si le User est contenu dans la collection de likes d'utilisateurs du commentaire en question
+        if($memorial->isSupportedByUser($user))
+        {
+            // Si il a déjà liké, on lui dit de remove le like
+            $memorial->removeSoutient($user);
+            $manager->flush();
+
+            // On précise les datas que l'on souhaite envoyer au json
+            return $this->json([
+                'message' => 'Le like de commentaire a été supprimé.',
+                'nbLike' => $memorial->howManySupports()
+            ]);
+        }
+
+        $memorial->addSoutient($user);
+        $manager->flush();
+
+        return $this->json([
+            'message' => 'Le like de commentaire a été ajouté.',
+            'nbLike' => $memorial->howManySupports()
         ]);
     }
 }
