d84a4f505c145aaa3867e8f0abdd531c5daa4bfc
mayados <115099504+mayados@users.noreply.github.com>
2023-03-27 12:47:38 +0200

    form for Condoleance entity

---
+++ src/Form/CondoleanceType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Condoleance;
+use Symfony\Component\Form\AbstractType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class CondoleanceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('texte', CKEditorType::class, [
+            'constraints' => [
+                new Assert\NotBlank(['message' => 'Le message de condoléance ne peut pas être vide']),
+            ]
+        ])
+        ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Condoleance::class,
+        ]);
+    }
+}
