4e39a92ced70ec946b94ac2771391401a595b333
mayados <115099504+mayados@users.noreply.github.com>
2023-03-30 17:07:59 +0200

    modify code for better performances

--- public/js/modules/Filter.js
+++ public/js/modules/Filter.js
@@ -19,86 +19,52 @@ export default class Filter {
     }
 
     bindEvents(){
-      // On sélectionne les inputs (on pourrait cinler le type checkbox, mais nos inputs sont que des checkbox)
-      this.form.querySelectorAll('input').forEach(input => {
+      // On sélectionne les inputs de type checkbox
+      this.form.querySelectorAll('input[type = checkbox]').forEach(input => {
+        
         input.addEventListener('change', this.loadForm.bind(this))
       })
-      // On sélectionne les select du formulaire
+      //On sélectionne les select du formulaire
       this.form.querySelectorAll('select').forEach(select => {
         select.addEventListener('change', this.loadForm.bind(this))
       })
-      // On sélectionne les input Texte du formulaire
+      // On sélectionne les input de type text
       this.form.querySelectorAll('input[type = text]').forEach(select => {
         select.addEventListener('input', this.loadForm.bind(this))
       })
-      // Pagination lors d'un clic
-      // this.pagination.querySelectorAll('a').forEach(a => {
-      //   a.addEventListener("click", e => {
-      //     e.preventDefault()
-      //     this.loadUrl(a.getAttribute('href'))
-      //     console.log('click pagination')
-      //   })
-      // })
-
-      // Quand on clique sur la pagination, si l'évènement a lieu sur une balise a, on effectue la requête en passant en url l'attribute du lien
-        this.pagination.addEventListener("click", e => {
-          if(e.target.tagName === 'a'){
-            e.preventDefault()
-            // console.log('click pagination')
-            this.loadUrl(a.getAttribute('href'))
-                        
-          }
-        })
-
     }
 
+    // function asynchrone
     async loadForm(){
+
       const data = new FormData(this.form)
-      // On crée une nouvelle URL à partir de ce qu'on retrouve dans le formulaire
-      // Si cet attribut n'existe pas, on récupère l'url courante
-      const url = new URL(this.form.getAttribute('action')|| window.location.href) 
-      console.log(url)
+      // On crée une nouvelle URL à partir de l'url courante
+      const url = new URL(window.location.href) 
       const params = new URLSearchParams()
+    
       data.forEach((value, key) => {
-        params.append(key, value)
+          params.append(key, value)          
       })
-      // return this.loadUrl(url.pathname + '?' + params.toString())
      
+      // On convertit en AJAX
       fetch(url.pathname + '?' + params.toString(), {
         headers: {
           //  Permet de différencier une requête classique d'une requête AJAX
           'X-Requested-With': 'XMLHttpRequest'
         }
       })
-      .then(async(response) =>{
-        const data = await response.json()
+      // Lorsque la première promesse (provenant de fetch) aboutit, elle nous donne une réponse (response) que l'on traite en JSON
+      .then(response => response.json())
+      // Lorsque la promesse provenant de .json aboutit, on récupère ce qui a été traité par le JSON
+      .then(data => {
+        // On remplace le contenu
         this.content.innerHTML = data.content
         this.pagination.innerHTML = data.pagination    
-        // Pour que l'utilisateur puisse retourner directement en arrière et n'ai pas à repasser par tous les filtres
-        history.replaceState({}, '', url)
+         // mettre à jour l'url
+        history.pushState({},null, url.pathname + "?" + params.toString());
       })
       .catch(e => alert(e));
 
     }
 
-    // On convertit en AJAX
-    // Async renvoie une promesse avec la valeur renvoyée, sinon s'il y a une exception elle sera rompue
-    // async loadUrl(url){
-      // const ajaxUrl = url + '&ajax=1'
-      // await permet de stopper l'exécution de la fonction asynchrone pour attendre la résolution de la promesse passée
-
-      // On vérifie si la requête a un statut situé entre 200 et 300, si c'est le cas tout se passe bien
-    //   if(response.status >= 200 && response.status < 300){
-    //     const data = await response.json()
-    //     this.content.innerHTML = data.content
-    //     this.pagination.innerHTML = data.pagination    
-    //     // Pour que l'utilisateur puisse retourner directement en arrière et n'ai pas à repasser par tous les filtres
-    //     history.replaceState({}, '', url)
-
-    //     // console.log(data)
-    //   }else{
-    //     console.error(response)
-    //   }
-    // }
-
-}
+}    
