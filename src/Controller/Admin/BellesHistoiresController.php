8104dbc815cab786b5288bc089afad8830f75e58
mayados <115099504+mayados@users.noreply.github.com>
2023-04-19 17:24:28 +0200

    add methods

--- src/Controller/Admin/BellesHistoiresController.php
+++ src/Controller/Admin/BellesHistoiresController.php
@@ -2,10 +2,9 @@
 
 namespace App\Controller\Admin;
 
-use App\Form\CategorieType;
-use App\Entity\CategorieAnimal;
+
 use Doctrine\Persistence\ManagerRegistry;
-use App\Repository\CategorieAnimalRepository;
+use App\Repository\BelleHistoireRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,6 +13,39 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class BellesHistoiresController extends AbstractController
 {
 
+    /*  Histoires publiées */
+    #[Route('/admin/histoires/publiees', name: 'app_admin_histoires_publiees')]
+    public function findHistoiresPubliees(BelleHistoireRepository $bhr, Request $request): Response
+    {
+        return $this->render('admin/publiees.html.twig', [
+            'histoiresPubliees' => $bhr->findPaginatedPubliees($request->query->getInt('page',1)),
+        ]); 
+    }
 
-    
+
+    /* Histoires en attente */
+    #[Route('/admin/histoires/attente', name: 'app_admin_histoires_attente')]
+    public function findHistoiresWaiting(BelleHistoireRepository $bhr, Request $request): Response
+    {
+        return $this->render('admin/en_attente.html.twig', [
+            'histoiresWaiting' => $bhr->findPaginatedWaiting($request->query->getInt('page',1)),
+        ]); 
+    }
+
+
+    /* Histoires désapprouvées */
+    #[Route('/admin/histoires/desapprouvees', name: 'app_admin_histoires_desapprouvees')]
+    public function findHistoiresDesapprouvees(BelleHistoireRepository $bhr, Request $request): Response
+    {
+        return $this->render('admin/desapprouvees.html.twig', [
+            'histoiresDesapprouvees' => $bhr->findPaginatedDisapprouved($request->query->getInt('page',1)),
+        ]); 
+    }
+
+
+    /* Liste des commentaires */
+
+
+    /* Genres (+ possibilité d'ajout) */
+
 }
