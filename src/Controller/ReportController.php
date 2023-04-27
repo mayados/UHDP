215b3f1e7bcd85d5bdf8ed4e74b62d2060c46cb0
mayados <115099504+mayados@users.noreply.github.com>
2023-04-27 13:45:53 +0200

    modify method's name and restrict controller to ROLE_USER

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -6,11 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+#[IsGranted('ROLE_USER')]
 class ReportController extends AbstractController
 {
-    #[Route('/report', name: 'app_report')]
-    public function index(): Response
+    #[Route('/report/memorial/{id}', name: 'app_report_memorial')]
+    public function reportMemorial(): Response
     {
+
+        /* On vérifie si le mémorial est dans la collection du user. Si c 'est le cas, on remove l'instance de classe ReportMmemorial */
+
+        /* S'il n'est pas, on crée une nouvelle instance de classe */
+
         return $this->render('report/index.html.twig', [
             'controller_name' => 'ReportController',
         ]);
