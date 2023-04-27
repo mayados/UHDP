6edabaed5eeaa6cd07b60a6fa45e52466541032b
mayados <115099504+mayados@users.noreply.github.com>
2023-04-27 12:21:05 +0200

    comment lines to delete many to many relationship

--- migrations/Version20230214143715.php
+++ migrations/Version20230214143715.php
@@ -31,14 +31,14 @@ final class Version20230214143715 extends AbstractMigration
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, pseudo VARCHAR(50) NOT NULL, photo VARCHAR(255) DEFAULT NULL, bannir TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE animal_memorial ADD CONSTRAINT FK_201B1CA923C92311 FOREIGN KEY (categorie_animal_id) REFERENCES categorie_animal (id)');
-        $this->addSql('ALTER TABLE animal_memorial ADD CONSTRAINT FK_201B1CA960BB6FE6 FOREIGN KEY (auteur_id) REFERENCES user (id)');
+        // $this->addSql('ALTER TABLE animal_memorial ADD CONSTRAINT FK_201B1CA960BB6FE6 FOREIGN KEY (auteur_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE belle_histoire ADD CONSTRAINT FK_DA312DEA60BB6FE6 FOREIGN KEY (auteur_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE comment_belle_histoire ADD CONSTRAINT FK_D2723C2360BB6FE6 FOREIGN KEY (auteur_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE comment_belle_histoire ADD CONSTRAINT FK_D2723C23B3C0CA49 FOREIGN KEY (belle_histoire_id) REFERENCES belle_histoire (id)');
-        $this->addSql('ALTER TABLE mot_commemoration ADD CONSTRAINT FK_9D6E7D4760BB6FE6 FOREIGN KEY (auteur_id) REFERENCES user (id)');
+        // $this->addSql('ALTER TABLE comment_belle_histoire ADD CONSTRAINT FK_D2723C23B3C0CA49 FOREIGN KEY (belle_histoire_id) REFERENCES belle_histoire (id)');
+        // $this->addSql('ALTER TABLE mot_commemoration ADD CONSTRAINT FK_9D6E7D4760BB6FE6 FOREIGN KEY (auteur_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE photo ADD CONSTRAINT FK_14B784187B40E4F7 FOREIGN KEY (memorial_id) REFERENCES animal_memorial (id)');
         $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8D60BB6FE6 FOREIGN KEY (auteur_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8D1F55203D FOREIGN KEY (topic_id) REFERENCES topic (id)');
+        // $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8D1F55203D FOREIGN KEY (topic_id) REFERENCES topic (id)');
         $this->addSql('ALTER TABLE topic ADD CONSTRAINT FK_9D40DE1B60BB6FE6 FOREIGN KEY (auteur_id) REFERENCES user (id)');
     }
 
