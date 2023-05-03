3e226d34a7f23e73e52238b631289c83d9413223
mayados <115099504+mayados@users.noreply.github.com>
2023-05-03 15:59:44 +0200

    create methods findMotsNonSignales() and findMotsSignales()

--- src/Controller/Admin/CommemorationController.php
+++ src/Controller/Admin/CommemorationController.php
@@ -8,6 +8,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use App\Repository\CategorieAnimalRepository;
 use Symfony\Component\HttpFoundation\Request;
 use App\Repository\MotCommemorationRepository;
+use App\Repository\ReportMotRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,13 +17,21 @@ class CommemorationController extends AbstractController
 {
 
     #[Route('/admin/commemoration/mots', name: 'app_admin_commemoration_mots')]
-    public function posts(MotCommemorationRepository $mcr, ManagerRegistry $doctrine, Request $request): Response
+    public function findMotsNonSignales(MotCommemorationRepository $mcr, ManagerRegistry $doctrine, Request $request): Response
     {
 
-        $mots = $mcr->findAll();
-
         return $this->render('admin/mur_commemoration/mots.html.twig', [
-            'mots' => $mots,
+            'mots' => $mcr->findPaginatedNonSignales($request->query->getInt('page',1)),
+        ]);  
+
+    }
+
+    #[Route('/admin/commemoration/mots/signales', name: 'app_admin_commemoration_mots_signales')]
+    public function findMotsSignales(ReportMotRepository $rmr, ManagerRegistry $doctrine, Request $request): Response
+    {
+
+        return $this->render('admin/mur_commemoration/mots_signales.html.twig', [
+            'mots' => $rmr->findPaginatedSignales($request->query->getInt('page',1)),
         ]);  
 
     }
