cd2a6e1327c9af9292beeda31eb984920f5dd491
mayados <115099504+mayados@users.noreply.github.com>
2023-05-04 11:15:48 +0200

    create UserPhotoType and add constraints

---
+++ src/Form/UserPhotoType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class UserPhotoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('imgUser', FileType::class, [
+                'label' => "Image de profil",
+                // Signifie que ce champ n'est pas associé à une propriété de l'entité AnimalMemorial
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    /* On ne peut pas définir la validation des champs non mappés 
+                    en utilisant des annontaions dans l'entité, On utilise donc les classes de contrainte php */
+                    new File([
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Veuillez choisir un format valide (jpeg, jpg, png)',
+                        'maxSize' => '300k',
+                        'maxSizeMessage' => "Image trop lourde, veuillez en sélectionner une autre.",
+                    ])
+                ]
+            ])
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
