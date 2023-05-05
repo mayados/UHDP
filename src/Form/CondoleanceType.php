23564b0a6b049d161c86bfb5178207e74c5b34ae
mayados <mariannedos.cours@gmail.com>
2023-05-05 20:31:09 +0200

    comment ckeditorType for test

--- src/Form/CondoleanceType.php
+++ src/Form/CondoleanceType.php
@@ -8,14 +8,21 @@ use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CondoleanceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('texte', CKEditorType::class, [
+        // ->add('texte', CKEditorType::class, [
+        //     'constraints' => [
+        //         new Assert\NotBlank(['message' => 'Le message de condoléance ne peut pas être vide']),
+        //     ]
+        // ])
+        ->add('texte', TextareaType::class, [
             'constraints' => [
                 new Assert\NotBlank(['message' => 'Le message de condoléance ne peut pas être vide']),
             ]
