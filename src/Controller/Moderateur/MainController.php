aa0def365323d510cabb8a8d283a512426b6a9b0
mayados <mariannedos.cours@gmail.com>
2023-05-08 11:55:59 +0200

    use security anotation to securise for multiple roles

--- src/Controller/Moderateur/MainController.php
+++ src/Controller/Moderateur/MainController.php
@@ -12,7 +12,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
+#[Security("is_granted('ROLE_MODERATEUR_HISTOIRES') or is_granted('ROLE_MODERATEUR_FORUM') or is_granted('ROLE_MODERATEUR_MEMORIAUX') or is_granted('ROLE_MODERATEUR_COMMEMORATION')", statusCode: 403)]
 class MainController extends AbstractController
 {
     #[Route('/moderateur', name: 'app_moderateur')]
--- src/Controller/Moderateur/UsersController.php
+++ src/Controller/Moderateur/UsersController.php
@@ -14,7 +14,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
+#[Security("is_granted('ROLE_MODERATEUR_HISTOIRES') or is_granted('ROLE_MODERATEUR_FORUM') or is_granted('ROLE_MODERATEUR_MEMORIAUX') or is_granted('ROLE_MODERATEUR_COMMEMORATION')", statusCode: 403)]
 class UsersController extends AbstractController
 {
     
