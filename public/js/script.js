4db65024e12e3a06b34d978e5fc34cc53c5e5719
mayados <115099504+mayados@users.noreply.github.com>
2023-05-26 17:24:36 +0200

    delete comments and import modules and functions

--- public/js/script.js
+++ public/js/script.js
@@ -3,13 +3,19 @@ import Filter from './modules/Filter.js'
 import Like from './modules/Like.js'
 import Favoris from './modules/Favoris.js'
 import Submit from './modules/Submit.js'
+// import Modify from './modules/Modify.js'
+import { addEventListenerToModifyBtn } from "./modules/ModifyButton.js";
+import { postModifyForm } from "./modules/Modify.js";
 
 
 // On passe l'élément que l'on veut rendre AJAX
 new Filter(document.querySelector('.js-filter'))
 
 new Submit(document.querySelector('.js-refresh-page'))
-// console.log(document.querySelector('.js-auto-refresh'))
+
+// new Modify(document.querySelector('.js-modify-page'))
+// console.log(document.querySelector('.js-modify-page'))
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -32,6 +38,12 @@ document.addEventListener('DOMContentLoaded', () => {
     if(favorisElements){
         new Favoris(favorisElements);
     }
+
+    // const modifyFormsElements = [].slice.call(document.querySelectorAll('.js-modify-form'))
+    // console.log(modifyFormsElements)
+    // if(modifyFormsElements){
+    //     new Modify(modifyFormsElements);
+    // }
 
     const recherche = document.querySelector('.recherche');
     if(recherche){
@@ -168,75 +180,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 
-    function addEventListenerToModifyBtn(){
-        const modifyButtons = document.querySelectorAll('.modify-button');
-        modifyButtons.forEach(function(button) {
-            button.addEventListener('click', function() {           
-                // console.log(this.id)
-                console.log("je suis cliqué")
-                const idButton = this.id;
-                CKEDITOR.replace( 'texte_edit'+idButton );
-                CKEDITOR.add 
-                const formulaire = document.querySelector("#form"+idButton);
-                const condoleanceTexte = document.querySelector("#condoleance"+idButton)
-                const formulaireClass = formulaire.className;
-                formulaire.classList.remove('modify-form');
-                formulaire.classList.add('modify-form-visible');
-                condoleanceTexte.style.display='none';
-                // console.log(formulaire)
-    
-                const idClose = document.querySelector("#close"+idButton);
-    
-                console.log(idClose)
-        
-                idClose.addEventListener('click', e =>{
-                    e.preventDefault();
-                    console.log(this.href)
-                    formulaire.classList.add('modify-form');
-                    formulaire.classList.remove('modify-form-visible')
-                    condoleanceTexte.style.display='block';
-                })  
-            })
-        })}
     addEventListenerToModifyBtn();
-   
-
-    // const modifyButtons = document.querySelectorAll('.modify-button');
-    // modifyButtons.forEach(function(button) {
-    //     button.addEventListener('click', function() {           
-    //         // console.log(this.id)
-    //         event.preventDefault();            
-    //         const idButton = this.id;
-
-    //         // console.log(this.getAttribute("formaction"))
-    //         // const actionUrl = this.getAttribute("formaction")
-    //         const url = new URL(this.formAction|| window.location.href);
-    //         // console.log(url)
-    //         fetch(url.pathname, {
-    //             headers: {
-    //             'X-Requested-With': 'XMLHttpRequest'
-    //             },
-    //             // method: 'GET'
-    //         })
-    //         .then(response => response.json())
-    //         .then(response => {
-
-    //             const form = response.formEditCondoleance
-    //             CKEDITOR.replace( 'form_texte' );
-    //             CKEDITOR.add 
-    //             // console.log(CKEDITOR.instances)
-    //             const containerForm = document.querySelector("#form"+idButton);  
-
-    //             containerForm.innerHTML = form
-    //             // containerForm.appendChild(form);
-    //             console.log(form)
-    //             containerForm.classList.remove('modify-form');
-    //             containerForm.classList.add('modify-form-visible');
-    //             // console.log(form)
-    //         })
-
-    //     })
-    // })
+    // postModifyForm();
 
 
 })
