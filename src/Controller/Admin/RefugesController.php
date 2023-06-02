b65fbff50e3aad4618a1b6360e3cd2ad6782a9fa
mayados <115099504+mayados@users.noreply.github.com>
2023-06-02 16:55:10 +0200

    add methods displayRefuges(), showRefuge() and removeRefuge()

--- src/Controller/Admin/RefugesController.php
+++ src/Controller/Admin/RefugesController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Refuge;
 use App\Form\RefugeType;
+use App\Repository\RefugeRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,28 +14,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class RefugesController extends AbstractController
 {
 
-    // #[Route('/admin/refuges/liste', name: 'app_admin_refuges_liste')]
-    // public function findReported(Request $request): Response
-    // {
-    //     // return $this->render('admin/messagerie/messages_signales.html.twig', [
-    //     //     'messagesSignales' => $mr->findPaginatedSignales($request->query->getInt('page',1)),
-    //     // ]); 
-    // }
-
-    #[Route('/admin/refuges/add', name: 'app_admin_refuges_add')]
-    #[Route('/admin/refuges/edit/{id}', name: 'edit_refuge')]
-    public function addRefuge(Refuge $refuge = null, Request $request, ManagerRegistry $doctrine): Response
+    #[Route('/admin/refuges/liste', name: 'app_admin_refuges_liste')]
+    public function displayRefuges(RefugeRepository $rr,Request $request): Response
     {
 
-        $edit = false;
-        if($refuge){
-            $edit = true;
-        // CREATION 
-        }elseif(!$refuge){
-            $refuge = new Refuge();      
-        }else{
-            return $this->redirectToRoute('app_admin_refuges_liste');
-        }
+        $refuges = $rr->findPaginatedRefugesForAdmin($request->query->getInt('page',1));
+
+        return $this->render('admin/refuges/refuges.html.twig', [
+            'refuges' => $refuges,
+        ]); 
+    }
+
+    #[Route('/admin/refuges/show/{id}', name: 'app_admin_show_refuge')]
+    public function showRefuge(Refuge $refuge, Request $request, ManagerRegistry $doctrine): Response
+    {
 
         $form = $this->createForm(RefugeType::class, $refuge);
         $form->handleRequest($request);
@@ -45,16 +38,56 @@ class RefugesController extends AbstractController
             $entityManager->persist($refuge);
             $entityManager->flush();
 
-            ($edit)?$this->addFlash('success', 'Le refuge a été modifié avec succès'):$this->addFlash('success', 'Le refuge a été créé avec succès');
+            $this->addFlash('success', 'Le refuge a été modifié avec succès');
+
+            return $this->redirectToRoute('app_admin_show_refuge',[
+                'id' => $refuge->getId(),
+            ]);
+        }
+
+        return $this->render('admin/refuges/showRefuge.html.twig', [
+            'formEditRefuge' => $form->createView(),
+            'refuge' => $refuge
+        ]);     
+
+    }
+
+    #[Route('/admin/refuges/add', name: 'app_admin_refuges_add')]
+    public function addRefuge(Request $request, ManagerRegistry $doctrine): Response
+    {
+
+        $refuge = new Refuge();
+
+        $form = $this->createForm(RefugeType::class, $refuge);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $refuge = $form->getData();            
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($refuge);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le refuge a été ajouté avec succès');
 
             return $this->redirectToRoute('app_admin_refuges_add');
         }
 
         return $this->render('admin/refuges/add.html.twig', [
             'formAddRefuge' => $form->createView(),
-            'edit' => $edit,
             'refuge' => $refuge
         ]);     
+
+    }
+
+    #[Route('/admin/refuge/remove/{id}', name: 'app_admin_refuge_remove')]
+    public function removeRefuge(Refuge $refuge, RefugeRepository $rr ,Request $request, ManagerRegistry $doctrine)
+    {
+
+        $rr->remove($refuge, $flush = true);
+
+        $this->addFlash('success','Le refuge a été supprimé avec succès');
+
+        return $this->redirectToRoute('app_admin_refuges_liste');     
 
     }
 
