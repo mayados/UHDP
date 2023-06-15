e8feaac76c15d614effc7722981511a1f9f2229f
mayados <mariannedos.cours@gmail.com>
2023-06-15 21:57:56 +0200

    add new use and add new RememberMeBadge() in function authenticate

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -15,6 +15,7 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 
 class AppAuthenticator extends AbstractLoginFormAuthenticator
 {
@@ -43,6 +44,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
             new PasswordCredentials($request->request->get('password', '')),
             [
                 new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
+                new RememberMeBadge(),
             ]
         );
     }
