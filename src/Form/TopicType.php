bc89db722ace34bebbc600ae516a0df727700d1f
mayados <mariannedos.cours@gmail.com>
2023-06-27 08:45:32 +0200

    change and add labels

--- src/Form/TopicType.php
+++ src/Form/TopicType.php
@@ -17,7 +17,7 @@ class TopicType extends AbstractType
     {
         $builder
             ->add('titre', TextType::class, [
-                'label' => 'Titre du sujet * (ne doit pas excéder 255 caractères)',
+                'label' => 'Titre du sujet * (maximum 255 caractères)',
                 'constraints' => [
                     new Assert\NotBlank(['message' => 'Le titre ne peut pas être vide']),
                     new Assert\Length(['min' => 2, 'max' => 255, 'minMessage' => 'Le titre doit faire au moins {{ limit }} caractères', 'maxMessage' => 'Le titre ne peut pas faire plus de {{ limit }} caractères'])
@@ -30,7 +30,9 @@ class TopicType extends AbstractType
                     new Assert\NotBlank(['message' => 'Le commentaire ne peut pas être nul']),
                 ]
             ])
-            ->add('submit', SubmitType::class)
+            ->add('submit', SubmitType::class, [
+                'label' => 'Soumettre',
+            ])
         ;
     }
 
