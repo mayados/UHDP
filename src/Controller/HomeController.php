f8db8dfff1a491f5d0a5116d11a9e654c237e6e5
mayados <mariannedos.cours@gmail.com>
2023-07-17 22:55:08 +0200

    add routes for annexes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -31,4 +31,25 @@ class HomeController extends AbstractController
         return $this->render('notreHistoire/notreHistoire.html.twig');
     }
 
+    #[Route('/mentions/legales', name: 'app_mentions_legales')]
+    public function showMentions()
+    {
+
+        return $this->render('annexes/mentions_legales.html.twig');
+    }
+
+    #[Route('/cgu', name: 'app_cgu')]
+    public function showCGU()
+    {
+
+        return $this->render('annexes/cgu.html.twig');
+    }
+
+    #[Route('/politique/confidentialite', name: 'app_politique_confidentialite')]
+    public function showPolitique()
+    {
+
+        return $this->render('annexes/politique_confidentialite.html.twig');
+    }
+
 }
